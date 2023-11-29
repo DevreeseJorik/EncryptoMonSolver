6de976e0b2b755e4e351399e6c8ce0cfcf20bdd1
Devreese Jorik <jorik.devreese@student.howest.be>
2023-11-29 13:17:13 +0100

    run solver in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
-#include "EncryptoMon.h"
+#include "EncryptoMonSolver.h"
 #include <iostream>
 
 int main() {
+    TweakConfig tConfig;
+    Config config;
+    EncryptoMonSolver solver;
     EncryptoMon encryptoMon;
-    Pokemon test;
+    Pokemon pokemon;
+
+    config.loadTweakEnvFile(R"(.env)", tConfig);
 
     // Load Pokemon data from binary file
-    if (encryptoMon.loadBinaryPokemon(R"(poke.bin)", test)) {
-        encryptoMon.processPokemon(test);
-        // Save encrypted Pokemon data to a new file
-        encryptoMon.dumpBinaryPokemon(R"(enc_poke.bin)", test);
+    //if (encryptoMon.loadBinaryPokemon(R"(poke.bin)", pokemon)) {
+    //    encryptoMon.setItem(pokemon, 0x1E);
+    //    encryptoMon.processPokemon(pokemon);
+    //    // Save encrypted Pokemon data to a new file
+    //    encryptoMon.dumpBinaryPokemon(R"(enc_poke.bin)", test);
+    //}
+
+    if (encryptoMon.loadBinaryPokemon(R"(poke.bin)", pokemon)) {
+        solver.startSearch(pokemon, encryptoMon, tConfig);
+
     }
 
     return 1;
